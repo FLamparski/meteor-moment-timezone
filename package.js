93e713ec0ab5b23a0b451daaee7a010ca75b9c7e
Filip Wieland <filiplamparski@gmail.com>
2014-08-11 16:17:06 +0100

    Upgrade moment-timezone

--- package.js
+++ package.js
@@ -3,13 +3,13 @@ Package.describe({
 });
 
 Package.on_use(function (api, where) {
-  where = where || ['client', 'server']
+  where = where || ['client', 'server'];
   api.use("moment", where);
   if (api.imply) {
     api.imply("moment", where);
   }
-  api.add_files('require-shim.js', where);
+  api.add_files('require-shim.js', 'server');
   api.add_files('lib/moment-timezone/moment-timezone.js', where);
-  api.add_files('lib/moment-timezone-data/moment-timezone-data.js', where);
-  api.add_files('revert-require-shim.js', where);
+  api.add_files('load_tz_data.js', where);
+  api.add_files('revert-require-shim.js', 'server');
 });
